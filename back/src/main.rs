2bfc6a624fe3d3d334789f03a18d223abba1716d
Yanis Guaye <yanis.guaye@epitech.eu>
2024-03-04 23:10:11 +0100

    feat(12): add `tracing` for logging backend events

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,8 +1,10 @@
 use std::io;
 
-extern crate actix_web;
-
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use tracing::{info, warn};
+use tracing_actix_web::TracingLogger;
+
+mod log;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -11,6 +13,7 @@ async fn hello() -> impl Responder {
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
+    warn!("somebody echoed us!");
     HttpResponse::Ok().body(req_body)
 }
 
@@ -19,14 +22,21 @@ async fn manual_hello() -> impl Responder {
 }
 
 #[actix_web::main]
+#[tracing::instrument]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
+    let _guard = log::init();
+
+    let address = "127.0.0.1";
+    let port = 8080;
+    let server = HttpServer::new(|| {
         App::new()
+            .wrap(TracingLogger::default())
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    .bind((address, port))?;
+
+    info!(address, port, "starting Pictou server");
+    server.run().await
 }
