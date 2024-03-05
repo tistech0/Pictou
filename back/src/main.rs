5e1c370208dbba0d43bd7592405acfccf73ec9da
BaptistePgn <62382812+BaptistePgn@users.noreply.github.com>
2024-03-05 11:57:31 +0100

    feat(6): create database (#13)
    Closes #6

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,6 +1,9 @@
+use std::env::var;
 use std::io;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use tokio_postgres::{Error, NoTls};
 use tracing::{info, warn};
 use tracing_actix_web::TracingLogger;
 
@@ -17,6 +20,35 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn connect_to_db() -> Result<(), Error> {
+    dotenv().ok();
+    let host = var("POSTGRES_HOST").expect("PG_HOST not set in .env");
+    let user = var("POSTGRES_USER").expect("PG_USER not set in .env");
+    let password = var("POSTGRES_PASSWORD").expect("PG_PASSWORD not set in .env");
+    let dbname = var("POSTGRES_DB").expect("PG_DATABASE not set in .env");
+
+    let conn_string = format!(
+        "host={} user={} password={} dbname={}",
+        host, user, password, dbname
+    );
+    let (client, connection) = tokio_postgres::connect(&conn_string, NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client.query("SELECT * FROM User", &[]).await?;
+
+    println!("{rows:?}");
+
+    Ok(())
+}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -25,9 +57,14 @@ async fn manual_hello() -> impl Responder {
 #[tracing::instrument]
 async fn main() -> io::Result<()> {
     let _guard = log::init();
-
-    let address = "127.0.0.1";
-    let port = 8080;
+    connect_to_db().await.expect("failed to connect to db");
+    dotenv().ok();
+    let port = var("PORT")
+        .expect("PORT not set in .env")
+        .as_str()
+        .parse::<u16>()
+        .expect("PORT is not a valid number");
+    let address = &*var("ADDRESS").expect("ADDRESS not set in .env");
     let server = HttpServer::new(|| {
         App::new()
             .wrap(TracingLogger::default())
