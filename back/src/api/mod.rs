8e87aa1293cb165c9c6ce9eec2f6105b4a1adaaf
Andreas Leroux <andreas.leroux@epitech.eu>
2024-03-11 11:12:06 +0100

    feat(18): Implement users endpoints

--- back/src/api/mod.rs
+++ back/src/api/mod.rs
@@ -1,9 +1,11 @@
 pub mod albums;
 pub mod images;
+pub mod users;
 use actix_web::web;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     //define your methods here
     cfg.service(web::scope("/images").configure(images::configure))
-        .service(web::scope("/albums").configure(albums::configure));
+        .service(web::scope("/albums").configure(albums::configure))
+        .service(web::scope("/users").configure(users::configure));
 }
