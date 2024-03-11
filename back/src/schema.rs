048c1e785c1132291a34e16975e43391da5989f3
BaptistePgn <baptiste.pgn@gmail.com>
2024-03-11 10:08:13 +0100

    feat: add diesel for migration

---
+++ back/src/schema.rs
@@ -0,0 +1,11 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        #[max_length = 255]
+        username -> Varchar,
+        #[max_length = 255]
+        password -> Varchar,
+    }
+}
