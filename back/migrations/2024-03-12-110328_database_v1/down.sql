bc2e62b9def858d15770789cd1e0babb93324f9d
BaptistePgn <baptiste.pgn@gmail.com>
2024-03-12 15:58:20 +0100

    feat: finish migration for database

--- back/migrations/2024-03-12-110328_database_v1/down.sql
+++ back/migrations/2024-03-12-110328_database_v1/down.sql
@@ -1,3 +1,7 @@
--- This file should undo anything in `up.sql`
+DROP TABLE shared_albums;
+DROP TABLE album_images;
+DROP TABLE albums;
+DROP TABLE user_images;
+DROP TABLE stored_images;
 DROP TABLE users;
 DROP TYPE auth_type;
--- back/migrations/2024-03-12-110328_database_v1/up.sql
+++ back/migrations/2024-03-12-110328_database_v1/up.sql
@@ -1,4 +1,3 @@
--- Your SQL goes here
 CREATE TYPE auth_type AS ENUM ('google', 'apple');
 
 CREATE TABLE users (
@@ -8,4 +7,45 @@ CREATE TABLE users (
   auth_type auth_type NOT NULL,
   token_exp TIMESTAMP NOT NULL,
   oauth_token VARCHAR(255) NULL
+);
+
+CREATE TABLE stored_images (
+    hash BYTEA PRIMARY KEY,
+    compression_alg VARCHAR(255) NOT NULL,
+    size INT NOT NULL,
+    width INT NOT NULL,
+    height INT NOT NULL
+);
+
+CREATE TABLE user_images (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    image_id BYTEA NOT NULL,
+    tags TEXT[] NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (image_id) REFERENCES stored_images(hash)
+);
+
+CREATE TABLE albums (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    owner_id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    tags TEXT[] NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE album_images (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    album_id UUID NOT NULL,
+    image_id BYTEA NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (image_id) REFERENCES stored_images(hash)
+);
+
+CREATE TABLE shared_albums (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    album_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
