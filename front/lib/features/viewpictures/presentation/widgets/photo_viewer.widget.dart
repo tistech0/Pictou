11d37f1d3b38e10ee3fb02e73c8bc983791dfdd0
Pacôme <dhiser.pacome@gmail.com>
2024-03-12 12:08:05 +0100

    style: add information button on focus picture view

--- front/lib/features/viewpictures/presentation/widgets/photo_viewer.widget.dart
+++ front/lib/features/viewpictures/presentation/widgets/photo_viewer.widget.dart
@@ -4,8 +4,8 @@ class PhotoViewer extends StatelessWidget {
   final List<String> imagePaths;
   final int initialIndex;
 
-  const PhotoViewer({Key? key, required this.imagePaths, this.initialIndex = 0})
-      : super(key: key);
+  const PhotoViewer(
+      {super.key, required this.imagePaths, this.initialIndex = 0});
 
   @override
   Widget build(BuildContext context) {
@@ -13,33 +13,47 @@ class PhotoViewer extends StatelessWidget {
       backgroundColor: Colors.transparent,
       child: GestureDetector(
         onTap: () => Navigator.of(context).pop(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Expanded(
-              child: PageView.builder(
-                itemCount: imagePaths.length,
-                controller: PageController(initialPage: initialIndex),
-                itemBuilder: (context, index) {
-                  return Image.asset(
-                    imagePaths[index],
-                    fit: BoxFit.contain,
-                  );
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconButton(
-                  icon: const Icon(Icons.share, color: Colors.white),
-                  onPressed: () {},
+                Expanded(
+                  child: PageView.builder(
+                    itemCount: imagePaths.length,
+                    controller: PageController(initialPage: initialIndex),
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        imagePaths[index],
+                        fit: BoxFit.contain,
+                      );
+                    },
+                  ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.delete, color: Colors.white),
-                  onPressed: () {},
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.share, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                  ],
                 ),
               ],
+            ),
+            Positioned(
+              top: 10,
+              right: 10,
+              child: IconButton(
+                icon: Icon(Icons.info_outline, color: Colors.white),
+                onPressed: () {
+                  // Action quand l'icône est pressée
+                },
+              ),
             ),
           ],
         ),
