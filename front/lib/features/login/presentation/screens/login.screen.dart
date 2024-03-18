4b1506fed93701d34505ff432d83283cea35bc2c
Pacôme <dhiser.pacome@gmail.com>
2024-03-18 09:35:56 +0100

    feat: add logo rounded on login page

--- front/lib/features/login/presentation/screens/login.screen.dart
+++ front/lib/features/login/presentation/screens/login.screen.dart
@@ -12,6 +12,15 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image.asset(
+                'assets/images/default_image.jpeg',
+                width: 150,
+                height: 150,
+              ),
+            ),
+            const SizedBox(height: 30),
             Text(
               'Welcome back on Pictou center',
               style: TextStyle(fontSize: 24),
@@ -23,8 +32,7 @@ class LoginScreen extends StatelessWidget {
               },
               child: Text(
                 'Login',
-                style:
-                    TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+                style: TextStyle(color: Colors.blue),
               ),
             ),
           ],
