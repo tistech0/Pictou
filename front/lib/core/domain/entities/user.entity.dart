e476c5e9cfd5eeb87fa98697472b33f25ce0ad7d
tistech <contact@matthislesur.com>
2024-03-25 12:17:11 +0100

    feat: create user provider and create user entity

---
+++ front/lib/core/domain/entities/user.entity.dart
@@ -0,0 +1,27 @@
+class UserEntity {
+  final String id;
+  String accessToken;
+  final String refreshToken;
+
+  UserEntity({
+    required this.id,
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  UserEntity copyWith({
+    String? id,
+    String? accessToken,
+    String? refreshToken,
+  }) {
+    return UserEntity(
+      id: id ?? this.id,
+      accessToken: accessToken ?? this.accessToken,
+      refreshToken: refreshToken ?? this.refreshToken,
+    );
+  }
+
+  void updateAccessToken({required String updatedAccessToken}) {
+    accessToken = updatedAccessToken;
+  }
+}
