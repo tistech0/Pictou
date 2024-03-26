fed04932f7d3266476fe325b247850d605bebcf1
Yanis Guaye <yanis.guaye@epitech.eu>
2024-03-26 13:24:54 +0100

    feat(75): add `GET /users/self` API route

--- back/src/api/users.rs
+++ back/src/api/users.rs
@@ -1,6 +1,13 @@
-use crate::{api::json_payload_error_example, auth::AuthContext, error_handler::ApiError};
+use crate::{
+    api::json_payload_error_example,
+    auth::AuthContext,
+    database::{self, Database, DatabaseError},
+    error_handler::ApiError,
+};
 use actix_web::{delete, get, patch, web, HttpResponse, Responder};
+use diesel::{Queryable, Selectable};
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
 use utoipa::ToSchema;
 use uuid::Uuid;
 
@@ -11,58 +18,58 @@ pub struct UserPost {
     name: String,
 }
 
-#[derive(Deserialize, Serialize, ToSchema)]
+#[derive(Deserialize, Serialize, ToSchema, Queryable, Selectable)]
+#[diesel(table_name = crate::schema::users)]
 pub struct User {
     id: Uuid,
-    name: String,
-}
-
-#[derive(Deserialize, Serialize, ToSchema)]
-pub struct UserList {
-    users: Vec<User>,
+    email: String,
+    name: Option<String>,
+    given_name: Option<String>,
+    family_name: Option<String>,
 }
 
 /// Get user (self) properties
-#[allow(unused_variables)]
-#[allow(unreachable_code)]
 #[utoipa::path(
     context_path = CONTEXT_PATH,
     responses(
-        (status = StatusCode::OK, description = "User's properties retrieved successfully", body = User, content_type = "application/json"),
-        (status = StatusCode::UNAUTHORIZED, description = "User not authenticated", body = ApiError, example = json!(ApiError::unauthorized_error()), content_type = "application/json"),
+        (
+            status = StatusCode::OK,
+            description = "User's properties retrieved successfully",
+            body = User,
+            content_type = "application/json"
+        ),
+        (
+            status = StatusCode::UNAUTHORIZED,
+            description = "User not authenticated",
+            body = ApiError,
+            example = json!(ApiError::unauthorized_error()),
+            content_type = "application/json"
+        ),
     ),
-    tag="users",
+    tag = "users",
     security(
         ("JWT Access Token" = [])
     )
 )]
 #[get("/self")]
-pub async fn get_self(auth: AuthContext) -> impl Responder {
-    todo!("Implement get_self method.");
-    HttpResponse::Ok().json(User {
-        id: Uuid::new_v4(),
-        name: "John Doe".to_string(),
-    })
-}
+pub async fn get_self(
+    auth: AuthContext,
+    db: web::Data<Database>,
+) -> Result<HttpResponse, ApiError> {
+    let user: User = database::open(db, move |conn| {
+        use crate::schema::users;
+        use diesel::prelude::*;
 
-/// Get a list of users
-#[allow(unused_variables)]
-#[allow(unreachable_code)]
-#[utoipa::path(
-    context_path = CONTEXT_PATH,
-    responses(
-        (status = StatusCode::OK, description = "Users retrieved successfully", body = UserList, content_type = "application/json"),
-        (status = StatusCode::UNAUTHORIZED, description = "User not authenticated", body = ApiError, example = json!(ApiError::unauthorized_error()), content_type = "application/json"),
-    ),
-    tag="users",
-    security(
-        ("JWT Access Token" = [])
-    )
-)]
-#[get("")]
-pub async fn get_users(auth: AuthContext) -> impl Responder {
-    todo!("Implement get_users method.");
-    HttpResponse::Ok().json(UserList { users: vec![] })
+        users::table
+            .filter(users::id.eq(auth.user_id))
+            .select(User::as_returning())
+            .get_result::<User>(conn)
+            .map_err(DatabaseError::<ApiError>::from)
+    })
+    .await
+    .map_err(ApiError::from)?;
+
+    Ok(HttpResponse::Ok().json(user))
 }
 
 /// Modify user (self) properties
@@ -71,11 +78,27 @@ pub async fn get_users(auth: AuthContext) -> impl Responder {
 #[utoipa::path(
     context_path = CONTEXT_PATH,
     responses(
-        (status = StatusCode::OK, description = "Successfully patched", body = User),
-        (status = StatusCode::BAD_REQUEST, description = "Invalid request data", body = ApiError, example = json!(json_payload_error_example()), content_type = "application/json"),
-        (status = StatusCode::UNAUTHORIZED, description = "User not authenticated", body = ApiError, example = json!(ApiError::unauthorized_error()), content_type = "application/json"),
+        (
+            status = StatusCode::OK,
+            description = "Successfully patched",
+            body = User
+        ),
+        (
+            status = StatusCode::BAD_REQUEST,
+            description = "Invalid request data",
+            body = ApiError,
+            example = json!(json_payload_error_example()),
+            content_type = "application/json"
+        ),
+        (
+            status = StatusCode::UNAUTHORIZED,
+            description = "User not authenticated",
+            body = ApiError,
+            example = json!(ApiError::unauthorized_error()),
+            content_type = "application/json"
+        ),
     ),
-    tag="users",
+    tag = "users",
     request_body(
         description = "User to edit",
         content_type = "application/json",
@@ -86,30 +109,41 @@ pub async fn get_users(auth: AuthContext) -> impl Responder {
     )
 )]
 #[patch("/self")]
-pub async fn edit_self(auth: AuthContext, patch: web::Json<UserPost>) -> impl Responder {
-    todo!("Implement edit_self method.");
-    HttpResponse::Ok().json(User {
-        id: Uuid::new_v4(),
-        name: "John Doe".to_string(),
+pub async fn edit_self(
+    auth: AuthContext,
+    patch: web::Json<UserPost>,
+    db: web::Data<Database>,
+) -> Result<impl Responder, ApiError> {
+    let user: User = database::open(db, move |conn| {
+        use crate::schema::users;
+        use diesel::prelude::*;
+
+        users::table
+            .filter(users::id.eq(auth.user_id))
+            .select(User::as_returning())
+            .get_result::<User>(conn)
+            .map_err(DatabaseError::<ApiError>::from)
     })
+    .await
+    .map_err(ApiError::from)?;
+
+    Ok(HttpResponse::Ok().json(user))
 }
 
 /// Delete the user account
-#[allow(unreachable_code)]
-#[allow(unused_variables)]
 #[utoipa::path(
     context_path = CONTEXT_PATH,
     responses(
         (status = StautsCode::NO_CONTENT, description = "Successfully deleted"),
         (status = StatusCode::UNAUTHORIZED, description = "User not authenticated", body = ApiError, example = json!(ApiError::unauthorized_error()), content_type = "application/json"),
     ),
-    tag="users",
+    tag = "users",
     security(
         ("JWT Access Token" = [])
     )
 )]
 #[delete("/self")]
-pub async fn delete_user(auth: AuthContext) -> impl Responder {
+pub async fn delete_user(auth: AuthContext, db: web::Data<Database>) -> impl Responder {
     todo!("Implement delete_user method.");
     HttpResponse::NoContent().finish()
 }
@@ -118,6 +152,5 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
     //define your methods here
     cfg.service(get_self)
         .service(edit_self)
-        .service(get_users)
         .service(delete_user);
 }
