f9b73e35b1e357c70d2345d1c0e3eee4ed3f554e
Yanis Guaye <yanis.guaye@epitech.eu>
2024-03-26 13:37:26 +0100

    feat(75): add `PATCH /users/self` API route

--- back/src/api/users.rs
+++ back/src/api/users.rs
@@ -5,18 +5,12 @@ use crate::{
     error_handler::ApiError,
 };
 use actix_web::{delete, get, patch, web, HttpResponse, Responder};
-use diesel::{Queryable, Selectable};
+use diesel::{AsChangeset, Queryable, Selectable};
 use serde::{Deserialize, Serialize};
-use time::OffsetDateTime;
 use utoipa::ToSchema;
 use uuid::Uuid;
 
 const CONTEXT_PATH: &str = "/users";
-
-#[derive(Deserialize, Serialize, ToSchema)]
-pub struct UserPost {
-    name: String,
-}
 
 #[derive(Deserialize, Serialize, ToSchema, Queryable, Selectable)]
 #[diesel(table_name = crate::schema::users)]
@@ -27,6 +21,20 @@ pub struct User {
     given_name: Option<String>,
     family_name: Option<String>,
 }
+
+#[derive(Deserialize, Serialize, ToSchema, AsChangeset, PartialEq, Eq)]
+#[diesel(table_name = crate::schema::users)]
+pub struct UserPatch {
+    name: Option<String>,
+    given_name: Option<String>,
+    family_name: Option<String>,
+}
+
+const EMPTY_USER_PATCH: UserPatch = UserPatch {
+    name: None,
+    given_name: None,
+    family_name: None,
+};
 
 /// Get user (self) properties
 #[utoipa::path(
@@ -102,7 +110,7 @@ pub async fn get_self(
     request_body(
         description = "User to edit",
         content_type = "application/json",
-        content = UserPost
+        content = UserPatch
     ),
     security(
         ("JWT Access Token" = [])
@@ -111,18 +119,30 @@ pub async fn get_self(
 #[patch("/self")]
 pub async fn edit_self(
     auth: AuthContext,
-    patch: web::Json<UserPost>,
+    patch: web::Json<UserPatch>,
     db: web::Data<Database>,
 ) -> Result<impl Responder, ApiError> {
+    let patch = patch.into_inner();
+
     let user: User = database::open(db, move |conn| {
         use crate::schema::users;
         use diesel::prelude::*;
 
-        users::table
-            .filter(users::id.eq(auth.user_id))
-            .select(User::as_returning())
-            .get_result::<User>(conn)
-            .map_err(DatabaseError::<ApiError>::from)
+        // Updating nothing is not an error
+        if patch != EMPTY_USER_PATCH {
+            diesel::update(users::table)
+                .set(patch)
+                .filter(users::id.eq(auth.user_id))
+                .returning(User::as_returning())
+                .get_result::<User>(conn)
+                .map_err(DatabaseError::<ApiError>::from)
+        } else {
+            users::table
+                .filter(users::id.eq(auth.user_id))
+                .select(User::as_returning())
+                .get_result::<User>(conn)
+                .map_err(DatabaseError::<ApiError>::from)
+        }
     })
     .await
     .map_err(ApiError::from)?;
