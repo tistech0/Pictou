d0f3fe408d0e3b8e62d1e2a22126493f4d44d8ee
Yanis Guaye <yanis.guaye@epitech.eu>
2024-04-16 02:04:02 +0200

    test(117): add testing database service and connection

--- back/tests/integration.rs
+++ back/tests/integration.rs
@@ -1,21 +1,55 @@
 use actix_web::web;
-use dotenv::dotenv;
-use pictou::config::AppConfiguration;
+use diesel::prelude::Insertable;
+use pictou::{config::AppConfiguration, database::TestingDatabase};
+use tokio::task::JoinHandle;
 
-#[ignore]
+#[derive(Insertable)]
+#[diesel(table_name = pictou::schema::users)]
+struct NewUser {
+    email: String,
+    name: Option<String>,
+}
+
+#[allow(dead_code)]
+struct IntegrationTestScope {
+    log_guard: pictou::log::Guard,
+    app_cfg: web::Data<AppConfiguration>,
+    server: JoinHandle<()>,
+    db: TestingDatabase,
+}
+
+impl IntegrationTestScope {
+    /// Sets up a full integration test environment.
+    async fn enter() -> anyhow::Result<Self> {
+        let log_guard = pictou::log::init();
+        dotenv::from_filename("test.env")?;
+
+        let app_cfg = web::Data::from(AppConfiguration::from_env()?);
+        let db = TestingDatabase::get(&app_cfg).await?;
+        db.truncate_all().await?;
+
+        let server = tokio::task::spawn({
+            let app_cfg = app_cfg.clone();
+            async {
+                pictou::start_server(app_cfg).await.unwrap();
+            }
+        });
+
+        Ok(Self {
+            log_guard,
+            app_cfg,
+            server,
+            db,
+        })
+    }
+}
+
 #[actix_rt::test]
 #[tracing::instrument]
-async fn start_server() {
-    let _guard = pictou::log::init();
+async fn experimental_test() -> anyhow::Result<()> {
+    let _scope = IntegrationTestScope::enter().await?;
 
-    dotenv().unwrap();
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
-    let app_cfg = web::Data::from(AppConfiguration::from_env().unwrap());
-
-    let _ = tokio::time::timeout(std::time::Duration::from_secs(1), async {
-        pictou::start_server(app_cfg).await.unwrap()
-    })
-    .await;
-
-    assert!(true)
+    Ok(())
 }
