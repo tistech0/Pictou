1e92828087b2c3abe0c81401739cef8ef70725cb
Matthou <contact@matthislesur.com>
2024-04-21 20:06:46 +0200

    feat: search by tag (#136)
    * feat: display metadata when click on info
    
    * fix: add new line at end of file photo_viewer.widget.dart
    
    * feat: delete image
    
    * feat: delete image
    
    * feat: delete image
    
    * feat: delete image
    
    * feat: delete image
    
    * fix: import pictures
    
    * fix: import pictures in bottom_bar.widget.dart
    
    * fix: new generate open_api files
    
    * fix: new generate open_api files
    
    * fix: new openapi generate file
    
    * fix: all feature working
    
    * feat: search by tag
    
    * fix: add new line end file
    
    * fix: add new line end file
    
    ---------
    
    Co-authored-by: Pacôme <dhiser.pacome@gmail.com>

--- front/lib/features/_global/presentation/widgets/app_bar.widget.dart
+++ front/lib/features/_global/presentation/widgets/app_bar.widget.dart
@@ -2,12 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:front/features/_global/presentation/widgets/import_pictures_dialog.widget.dart';
 import 'package:front/features/_global/presentation/widgets/new_album_dialog.widget.dart';
 import 'package:front/features/settings/presentation/screens/setting.screen.dart';
+import 'package:front/features/search/screens/viewPictureSearch.screen.dart';
+import 'package:pictouapi/pictouapi.dart';
+import 'package:provider/provider.dart';
+import 'package:pictouapi/src/model/images_meta_data.dart';
+
+import '../../../../core/config/userprovider.dart';
+
+class SearchBar extends StatefulWidget {
+  final Function(String) onSubmit;
+
+  SearchBar({required this.onSubmit});
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      decoration: InputDecoration(
+        hintText: 'Enter tags',
+        border: InputBorder.none,
+        suffixIcon: IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            widget.onSubmit(_controller.text);
+            _controller.clear();
+          },
+        ),
+      ),
+    );
+  }
+}
+
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final imageApi = Pictouapi().getImagesApi();
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.background,
@@ -56,6 +98,38 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
         title: const Text('PicTou'),
         actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: Text('Search'),
+                  content: SearchBar(
+                    onSubmit: (String tags) async {
+                      print(tags);
+                      final response = await imageApi.getImages(
+                        limit: 100,
+                        offset: 0,
+                        tags: tags,
+                        headers: {
+                          'Authorization': 'Bearer ${userProvider.user!.accessToken}',
+                        },
+                      );
+                      if (response.statusCode == 200) {
+                        final ImagesMetaData imagesMetaData = response.data!;
+                        final imageIds = imagesMetaData.images.map((e) => e.id).toList();
+                        print(imageIds);
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => ViewPicturesSearchScreen(imageIds: imageIds)));
+                      } else {
+                        print(response.statusMessage);
+                      }
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
